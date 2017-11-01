c2fb46b476a4cb9c7233d14cd243793b0a92fdde
Saviobosco <Johnboscoomebe@yahoo.com>
2017-11-01 13:02:46 +0100

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,9 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h3> Example Component </h3>
+        <div>
+            <h3> Example Component! </h3>
+        </div>
     )
 };
 
